8889cdc2b4e1e38af0f671f28c0cc6c8a48a9b77
Marcos <m@m.com>
2021-07-17 23:08:06 -0300

    Versão "rascunhada" das estruturas de dados em notação polonesa reversa e métodos para conversão e cálculo

---
+++ protobuftutorial2/src/main/java/br/ufc/mdcc/AT04/CalculatorAux.java
@@ -0,0 +1,25 @@
+package br.ufc.mdcc.AT04;
+
+public class CalculatorAux implements ICalculator {
+
+	@Override
+	public double soma(double oper1, double oper2) {
+		return oper1 + oper2;
+	}
+
+	@Override
+	public double subtrai(double oper1, double oper2) {
+		return oper1 - oper2;
+	}
+
+	@Override
+	public double multiplica(double oper1, double oper2) {
+		return oper1 * oper2;
+	}
+
+	@Override
+	public double divide(double oper1, double oper2) {
+		return oper1 / oper2;
+	}
+
+}
