285c9195e24c7eaafa237c8eb3a528ef0cc2c83b
otonp <otonpneto@gmail.com>
2021-07-21 22:44:22 -0300

    Generating xml file

--- protobuftutorial2/src/main/java/br/ufc/mdcc/AT04/client/xml/CalculadoraClientXML.java
+++ protobuftutorial2/src/main/java/br/ufc/mdcc/AT04/client/xml/CalculadoraClientXML.java
@@ -1,19 +1,112 @@
 package br.ufc.mdcc.AT04.client.xml;
 
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
 import br.ufc.mdcc.AT04.client.AbstractCalculadoraClient;
 import br.ufc.mdcc.AT04.shared.model.Element;
+import br.ufc.mdcc.AT04.shared.model.Number;
+import br.ufc.mdcc.AT04.shared.model.Operator;
+import br.ufc.mdcc.AT04.shared.protobuffer.RPNProto.MElement.EnumOperator;
 
 /**
  * Cliente para o serviço de calculadora disponibilizado via sockets.
  */
 public class CalculadoraClientXML extends AbstractCalculadoraClient {
-
+	
+	public static final String xmlFilePath = "D:\\xmlfile.xml";
+	
 	public CalculadoraClientXML(String serverAddr, int serverPort, String expression) {
 		super(serverAddr, serverPort, expression);
+	}
+	
+	/*
+	 * Given a List of Elements, generates an XML of expression
+	 */
+	private static StreamResult generateXMLMessage(
+			List<Element> elements) {
+		DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+		 
+        DocumentBuilder documentBuilder = null;
+		try {
+			documentBuilder = documentFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+        Document document = documentBuilder.newDocument();
+
+        // root element
+        org.w3c.dom.Element root = document.createElement("expression");
+        document.appendChild(root);
+
+		for (Element element : elements) {
+
+			if (element.isNumber()) {
+				Number number = (Number) element;
+
+				org.w3c.dom.Element numberXML = document.createElement("number");
+	            numberXML.appendChild(document.createTextNode(String.valueOf(number.getValue())));
+	            root.appendChild(numberXML);
+				
+			} else if (element.isOperator()) {
+				Operator operator = (Operator) element;
+				org.w3c.dom.Element operatorXML = document.createElement("operator");
+	            
+	            
+				if (operator.isPlusSign()) {
+					operatorXML.appendChild(document.createTextNode(String.valueOf(EnumOperator.ADDITION)));
+				} else if (operator.isMinusSign()) {
+					operatorXML.appendChild(document.createTextNode(String.valueOf(EnumOperator.SUBTRACTION)));
+				} else if (operator.isTimesSign()) {
+					operatorXML.appendChild(document.createTextNode(String.valueOf(EnumOperator.MULTIPLICATION)));
+				} else if (operator.isDividedBySign()) {
+					operatorXML.appendChild(document.createTextNode(String.valueOf(EnumOperator.DIVISION)));
+				}
+				root.appendChild(operatorXML);
+			}
+
+		}
+		// create the xml file
+        //transform the DOM Object to an XML File
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+		try {
+			transformer = transformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e1) {
+			e1.printStackTrace();
+		}
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(xmlFilePath));
+
+        // If you use
+        // StreamResult result = new StreamResult(System.out);
+        // the output will be pushed to the standard output ...
+        // You can use that for debugging 
+
+        try {
+			transformer.transform(domSource, streamResult);
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+
+        System.out.println("Done creating XML File");
+		return streamResult;
 	}
 
 	protected double receiveResultData(Socket clientSocket) throws IOException {
@@ -22,8 +115,11 @@ public class CalculadoraClientXML extends AbstractCalculadoraClient {
 	}
 
 	protected void sendRpnData(Socket clientSocket, List<Element> elements) throws IOException {
-		// TODO
-		return;
+		DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
+		// generates the xml message
+		StreamResult xmlMessage = generateXMLMessage(elements);
+		//socketSaidaServer.writeUTF();;
+		//socketSaidaServer.flush();
 	}
 
 	public static void main(String[] args) {
