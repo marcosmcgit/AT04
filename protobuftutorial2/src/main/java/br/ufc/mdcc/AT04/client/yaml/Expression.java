7122d1604fc6a5b15615e774d1ce8fbedfaff0d8
lailson azevedo <lailson93@gmail.com>
2021-07-22 00:49:44 -0300

    implementing Yaml Client side with Jacksons lib

---
+++ protobuftutorial2/src/main/java/br/ufc/mdcc/AT04/client/yaml/Expression.java
@@ -0,0 +1,22 @@
+package br.ufc.mdcc.AT04.client.yaml;
+
+import java.util.List;
+
+public class Expression {
+	private List<String> expressioElements;
+
+	public Expression(List<String> expressioElements) {
+		this.expressioElements = expressioElements;
+	}
+
+	public List<String> getExpressioElements() {
+		return expressioElements;
+	}
+
+	public void setExpressioElements(List<String> expressioElements) {
+		this.expressioElements = expressioElements;
+	}
+
+	// Without a default constructor, Jackson will throw an exception
+    public Expression() {}
+}
